4b805ae9b265c7e0d277d960d0d6b28c6db8e0cc
mouryaaakash45 <124234936+mouryaaakash45@users.noreply.github.com>
2023-07-04 16:31:03 +0530

    Add files via upload

---
+++ VariableDeclaration.java
@@ -0,0 +1,15 @@
+//Q5.Write a Java program to declare and initialize variables of different data types (int, double, String) with appropriate identifiers.
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        // declare the variable in different datatypes
+        String name = "Annu kumari";
+        double height = 5.2;
+        int age = 23;
+        
+        // print the value of variables
+        System.out.println("Name :" + name);
+        System.out.println("Height:" + height);
+        System.out.println("Age :" + age);
+
+    }
+}
